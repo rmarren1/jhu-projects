19ab84d9270926835aa2364a7f033cde8d79aa44
Ryan Marren <rmarren1@jhu.edu>
2016-12-05 21:55:46 -0500

    added cluster variation measurements, more plotting utilities

--- src/algos/similarity.py
+++ src/algos/similarity.py
@@ -28,12 +28,42 @@ def dtw(x):
 		return d.get_dist()
 	return distance_matrix(x, metric)
 
+def tsap(x):
+	d = x.shape[0]
+	T = x.shape[1]
+	out = []
+	for t in range(3, T):
+		s = np.empty([d, d])
+		for i in range(d):
+			for j in range(d):
+				v = 0
+				v += np.abs(1 + x[i, t - 3] - x[j, t - 3]) * (1.0/2**3)
+				v += np.abs(1 + x[i, t - 2] - x[j, t - 2]) * (1.0/2**2)
+				v += np.abs(1 + x[i, t - 1] - x[j, t - 1]) * (1.0/2)
+				v += np.abs(1 + x[i, t] - x[j, t])
+				s[i][j] = v
+		s = -s
+		out.append(np.array(ap(s)).T)
+	return (np.column_stack(out))
+
+def n_clusts(x):
+	return np.array(map(lambda y: np.unique(y).shape[0], x.T))
+
+def avg_clust_size(x):
+	return np.array(map(lambda y: np.mean(np.bincount(y)),x.T))
+
+def max_clust_size(x):
+	return np.array(map(lambda y: np.max(np.bincount(y)),x.T))
+
+def min_clust_size(x):
+	return np.array(map(lambda y: np.min(np.bincount(y)),x.T))
+
 def ap(x):
 	af = AffinityPropagation(affinity='precomputed')
 	af.fit(x)
 	labs = af.labels_
-	k = len(labs)
-	clusters = [[] for _ in range(np.max(labs) + 1)]
-	for i in range(k):
-		clusters[labs[i]].append(i)
-	return clusters
+	#k = len(labs)
+	#clusters = [[] for _ in range(np.max(labs) + 1)]
+	#for i in range(k):
+	#	clusters[labs[i]].append(i)
+	return labs
--- src/util/viz.py
+++ src/util/viz.py
@@ -3,16 +3,33 @@ import numpy as np
 import pandas as pd
 import cufflinks as cl
 
-def heatmap_channels(data, num, title = '', xtitle='', ytitle='', ztitle=''):
+def heatmap_channels(data, num, title = '', xtitle='', ytitle='', ztitle='', T = True):
     full_ind = range(len(data))
     ind = np.random.choice(full_ind, num, replace=False)
+    d = None
     for i in ind:
-        df = pd.DataFrame(data= data[i].T)
+        if T:
+            d = data[i].T
+        else:
+            d = data[i]
+        df = pd.DataFrame(data= data[i])
         fig = df.iplot(asFigure=True,
                     kind='heatmap',
                     xTitle=xtitle,
                     yTitle=ytitle,
-                    title=title,
-                    colorscale='blues')
+                    title=title)
         fig.data.update(dict(colorbar=dict(title=ztitle, titleside="right")))
+        iplot(fig)
+
+
+def line(data, num, title = '', xtitle='', ytitle='', ztitle=''):
+    full_ind = range(len(data))
+    ind = np.random.choice(full_ind, num, replace=False)
+    for i in ind:
+        df = pd.DataFrame(data = data[i])
+        fig = df.iplot(asFigure=True,
+                        kind='line',
+                        xTitle=xtitle,
+                        yTitle=ytitle,
+                        title=title)
         iplot(fig)
